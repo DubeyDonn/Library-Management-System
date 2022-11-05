1394961b67c986da4b30d6ce2c563d5181d2f4b0
DubeyDonn <nirajshrestha539@gmail.com>
2022-11-06 04:15:18 +0545

    unsaved added

--- admin_page.php
+++ admin_page.php
@@ -21,20 +21,50 @@ if (!isset($_SESSION['admin_name'])) {
 </head>
 
 <body>
-	<div class="container">
-
-		<div class="content">
-			<h3>hi, <span>admin</span></h3>
-			<h1>welcome <span>
+	<center>
+		<h1><u>ADMIN<span>
 					<?php echo $_SESSION['admin_name'] ?>
-				</span></h1>
-			<p>this is an admin page</p>
-			<a href="login_form.php" class="btn">login</a>
-			<a href="register_form.php" class="btn">register</a>
-			<a href="logout.php" class="btn">logout</a>
-		</div>
+				</span></u></h1>
+	</center>
+	<hr>
+	<ol>
+		<a href="5add book.html">
+			<li><button>ADD BOOK</button></li>
 
+			<br>
+			<a href="6book record.html">
+				<li><button>BOOK REPORT</button></li>
+
+				<br>
+				<a href="7book request.html">
+					<li><button>BOOK REQUEST</button></li>
+
+
+					<br>
+
+					<a href="addperson.html">
+						<li><button>ADD PERSON</button></li>
+
+						<br>
+						<a href="9student record.html">
+							<li><button>STUDENT RECORD</button></li>
+
+							<br>
+							<a href="10issue book.html">
+								<li><button>ISSUE BOOK</button></li>
+
+								<br>
+								<a href="11issue book record.html">
+									<li><button>ISSSUE BOOK REPORT</button></li>
+
+									<br>
+
+	</ol>
+	<div>
+		<button>LOGOUT</button>
 	</div>
+	<br>
+	<a href="admin.html"><input type="submit" name="submit" value="BACK"></a>
 
 </body>
 
--- config.php
+++ config.php
@@ -1,25 +1,5 @@
-<!-- <
-
-$dbname ="mysql:host=localhost; dbname=User_db2";
-$user='root';
-$password='';
-$option= array(
-    PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8'
-);
-try{
-    $connection = new PDO($dbname,$user,$pass, $option);
-    $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-}
-
-catch(PDOException $er){
-    echo $er;
-    exit();
-}
-? -->
-
 <?php
 
-$conn = mysqli_connect('localhost','root','','user_db');
+$conn = mysqli_connect('localhost', 'root', '', 'user_db');
 
 ?>
